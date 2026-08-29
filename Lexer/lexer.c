5015171c4895f6f087e4ca21844788cc94514153
David Florim <davidflo279@gmail.com>
2026-08-29 09:25:07 -0400

    Created ArrayList Library

--- Lexer/lexer.c
+++ Lexer/lexer.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "lexer.h"
+#include "ArrayList.h"
 
-#define INITIAL_LIST_LENGTH 8
-
-struct ArrayList
-{
-    void *array;
-    size_t length;
-    size_t size;
-    size_t element_size;
-};
+#define MAX_INTEGER 2147483647
 
 struct LexicalToken *create_token(int program_line, enum TOKEN_TYPE type)
 {
@@ -25,48 +16,6 @@ struct LexicalToken *create_token(int program_line, enum TOKEN_TYPE type)
     t->token_literal = NULL;
     t->type = type;
     return t;
-}
-
-void initialize_list(struct ArrayList *list, size_t element_size)
-{
-    list->array = malloc(INITIAL_LIST_LENGTH * element_size);
-    if (list->array == NULL)
-    {
-        printf("Malloc failed to allocate space\n");
-        /* Change Number */ exit(1);
-    }
-    list->length = 0;
-    list->size = INITIAL_LIST_LENGTH;
-    list->element_size = element_size;
-}
-
-void realloc_list(struct ArrayList *list, size_t new_size)
-{
-    void *new_loc;
-    size_t list_byte_length = new_size * list->element_size;
-    list->size = new_size;
-
-    new_loc = realloc(list->array, list_byte_length);
-    if (new_loc == NULL)
-    {
-        new_loc = malloc(list_byte_length);
-        if (new_loc == NULL)
-        {
-            printf("Malloc failed to allocate space\n");
-            /* Change Number */ exit(1);
-        }
-        memcpy(new_loc, list->array, list_byte_length);
-        free(list->array);
-    }
-    list->array = new_loc;
-}
-
-void append_list(struct ArrayList *list, void *element)
-{
-    if (list->length == list->size)
-        realloc_list(list, list->size * 2);
-    memcpy(list->array + list->length * list->element_size, &element, list->element_size);
-    list->length++;
 }
 
 int str_equal(const char *str1, const char *str2) {
@@ -216,7 +165,7 @@ struct LexicalToken **lex(FILE *source)
                 append_list(&new_string, (void *)(long long){c});
             } while ((c = fgetc(source)) >= '0' && c <= '9');
             append_list(&new_string, '\0');
-            if (new_string.length > 10 || (new_string.length == 10 && strtol(new_string.array, NULL, 10) > INT_MAX))
+            if (new_string.length > 10 || (new_string.length == 10 && strtol(new_string.array, NULL, 10) > MAX_INTEGER))
             {
                 printf("ERROR %d: Lexer: invalid integer %s\n", program_line, new_string.array);
             }
--- Scanner/scanner.c
+++ Scanner/scanner.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include "lexer.h"
 
-const char * const token_to_string[] = {
+#define FLAG_COUNT 1
+
+const char *const token_to_string[] = {
     [AT] = "AT",
     [CASE] = "CASE",
     [CLASS] = "CLASS",
@@ -45,32 +47,64 @@ const char * const token_to_string[] = {
     [THEN] = "THEN",
     [TRUE] = "TRUE",
     [TYPE] = "TYPE",
-    [WHILE] = "WHILE"
+    [WHILE] = "WHILE"};
+
+enum flags
+{
+    NORMAL,
+    OUTPUT_LEX
 };
 
-int main(int argc, char *argv[]) {
+const char *string_flags[] = {
+    "--lex"};
+
+// Returns 0 if not a flag otherwise returns enum flag integer
+int is_a_flag(const char *input)
+{
+    int i, j;
+    for (i = 0; i < FLAG_COUNT; i++)
+    {
+        for (j = 0; input[j] == string_flags[i][j] && input[j] != '\0'; j++)
+            ;
+        if (input[j] == string_flags[i][j])
+            return i + 1;
+    }
+    return NORMAL;
+}
+
+int main(int argc, char *argv[])
+{
     FILE *input_file;
     FILE *output_file;
-    if (argc < 2) {
+    int flag;
+    if (argc < 2)
+    {
         printf("No source file given\n");
         return 1;
     }
-    input_file = fopen(argv[1], "r");
-    if (input_file == NULL) {
+    flag = is_a_flag(argv[1]);
+    input_file = fopen(argv[flag == NORMAL ? 1 : 2], "r");
+    if (input_file == NULL)
+    {
         perror("Source file does not exist");
         return 2;
     }
-    if (fgetc(input_file) == EOF) {
+    if (fgetc(input_file) == EOF)
+    {
         printf("Source file is empty\n");
         return 3;
     }
     rewind(input_file);
     struct LexicalToken **output = lex(input_file);
-    output_file = fopen("output.txt", "w");
-    for (int i = 0; output[i] != NULL; i++) {
-        fprintf(output_file, "%d\n%s\n", output[i]->program_line, token_to_string[output[i]->type]);
-        if (output[i]->token_literal != NULL)
-            fprintf(output_file, "%s\n", output[i]->token_literal);
+    if (flag == OUTPUT_LEX)
+    {
+        output_file = fopen("output.txt", "w");
+        for (int i = 0; output[i] != NULL; i++)
+        {
+            fprintf(output_file, "%d\n%s\n", output[i]->program_line, token_to_string[output[i]->type]);
+            if (output[i]->token_literal != NULL)
+                fprintf(output_file, "%s\n", output[i]->token_literal);
+        }
     }
     return 0;
 }
